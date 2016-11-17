d37647b2fc50de13d9875aae676905502088b169
Jordan Wall <Jordan Wall>
2016-11-16 18:35:59 -0600

    Changed popup to say Jordan Wall

--- controllers/default.py
+++ controllers/default.py
@@ -17,7 +17,7 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
-    response.flash = T("Hello World")
+    response.flash = T("Jordan Wall")
     return dict(message=T('Welcome To CollaboR8 an online music sharing experience!'))
 
 
